c5acb8bd8c4ca77525ef35b19f1df12d937afc8c
Leandro Gavidia <leandro.d3v@gmail.com>
2025-07-27 16:22:33 -0400

    config: add fonts

--- app/src/main/java/com/example/boostbonk/ui/theme/Type.kt
+++ app/src/main/java/com/example/boostbonk/ui/theme/Type.kt
@@ -2,9 +2,21 @@ package com.example.boostbonk.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.example.boostbonk.R
+
+val Herborn = FontFamily(
+    Font(R.font.herborn)
+)
+
+val Poppins = FontFamily(
+    Font(R.font.poppins_regular),
+    Font(R.font.poppins_bold, FontWeight.Bold)
+)
+
 
 // Set of Material typography styles to start with
 val Typography = Typography(
